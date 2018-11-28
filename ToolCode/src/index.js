d4a208e6c1dece23e7eb3d0972094d8b6115f948
V16KMohamed29 <karim.othman-mohamed@vodafone.com>
2018-11-28 19:03:21 +0200

    CSS done

--- ToolCode/src/index.js
+++ ToolCode/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
+//import App from './components/app';
+import StormTrial from './components/StormTrial';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <div className='BackGround'>
+     <StormTrial />
+    </div>
   </Provider>
   , document.querySelector('.container'));
--- ToolCode/webpack.config.js
+++ ToolCode/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     loaders: [
       {
         exclude: /node_modules/,
-        loader: 'babel',
+        loader:'babel',
         query: {
           presets: ['react', 'es2015', 'stage-1']
         }
